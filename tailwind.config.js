a281f1aac589b14cdaf4e6a574a06a0d27646e48
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-06-16 23:07:14 +0700

    update home page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "banner-home": "url('/images/banner.webp')",
+      },
+    },
+  },
+  plugins: [],
+};
