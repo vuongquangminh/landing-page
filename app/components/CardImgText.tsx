a8a7d9e13453439f8f31b554b91ee009a57b1fef
vuongquangminh120802@gmail.com <vuongquangminh120802@gmail.com>
2025-06-26 22:26:03 +0700

    responsive feature chapter

--- app/components/CardImgText.tsx
+++ app/components/CardImgText.tsx
@@ -1,4 +1,4 @@
-import { Col } from "antd";
+import { Col, Row } from "antd";
 import Image from "next/image";
 import React from "react";
 
@@ -7,25 +7,60 @@ export default function CardImgText({
   title,
   content,
   img,
+  gather,
 }: {
   span: number;
   title: string;
   content: string;
   img: string;
+  gather?: {
+    id: number;
+    avatar: string;
+    title: string;
+    content: string;
+    subTitle: string;
+  }[];
 }) {
   return (
-    <Col span={span} className="relative overflow-hidden ">
-      <div className="absolute bottom-0 text-white p-6">
-        <div className="tracking-[3px] uppercase">{title}</div>
-        <div className="text-xl font-bold">{content}</div>
-      </div>
-      <Image
-        src={img}
-        alt="feature new"
-        width={800}
-        height={800}
-        className="h-full object-cover"
-      />
+    <Col
+      md={span * 2}
+      lg={span}
+      className=" overflow-hidden p-2 !flex flex-col"
+    >
+      <Row className="grow relative">
+        <div className="absolute bottom-0 text-white p-6">
+          <div className="tracking-[3px] uppercase">{title}</div>
+          <div className="text-xl font-bold">{content}</div>
+        </div>
+        <Image
+          src={img}
+          alt="feature new"
+          width={800}
+          height={800}
+          className=" h-[400px] object-cover"
+        />
+      </Row>
+      <Row className="w-full">
+        {gather?.map((item) => (
+          <Col
+            key={item.id}
+            span={24 / gather.length}
+            className="pt-4 !flex !items-center"
+          >
+            <Image
+              src={item.avatar}
+              alt="feature new"
+              width={80}
+              height={80}
+              className="rounded-full"
+            />
+            <div className="px-5 text-black">
+              <div className="tracking-[3px] uppercase">{item.title}</div>
+              <div className="text-base font-bold">{item.content}</div>
+            </div>
+          </Col>
+        ))}
+      </Row>
     </Col>
   );
 }
--- app/components/FeaturedNews.tsx
+++ app/components/FeaturedNews.tsx
@@ -1,33 +1,22 @@
-import { Col, Row } from "antd";
+import { Row } from "antd";
 import { MoveUpRight } from "lucide-react";
-import Image from "next/image";
 import React from "react";
 import CardImgText from "./CardImgText";
-type featuredNewType =
-  | {
-      id: number;
-      title: string;
-      content: string;
-      span: number;
-      img: string;
-    }
-  | {
-      id: number;
-      children: {
-        id: number;
-        title: string;
-        content: string;
-        span: number;
-        img: string;
-      }[];
-      gather: {
-        id: number;
-        avatar: string;
-        title: string;
-        content: string;
-        subTitle: string;
-      }[];
-    };
+type featuredNewType = {
+  id: number;
+  title: string;
+  content: string;
+  span: number;
+  img: string;
+  gather: {
+    id: number;
+    avatar: string;
+    title: string;
+    content: string;
+    subTitle: string;
+  }[];
+};
+
 export default function FeaturedNews() {
   const featuredNew: featuredNewType[] = [
     {
@@ -76,24 +65,11 @@ export default function FeaturedNews() {
     },
     {
       id: 3,
-      children: [
-        {
-          id: 1,
-          title: "3 Min Read",
-          img: "/images/feature-3.webp",
-          content:
-            "NASA Engineers Simulate Lunar Lighting for Artemis III Moon Landing",
-          span: 6,
-        },
-        {
-          id: 2,
-          title: "3 Min Read",
-          img: "/images/feature-4.jpg",
-          content:
-            "NASA Engineers Simulate Lunar Lighting for Artemis III Moon Landing",
-          span: 6,
-        },
-      ],
+      title: "3 Min Read",
+      img: "/images/feature-3.webp",
+      content:
+        "NASA Engineers Simulate Lunar Lighting for Artemis III Moon Landing",
+      span: 6,
       gather: [
         {
           id: 1,
@@ -116,73 +92,19 @@ export default function FeaturedNews() {
           </div>
         </div>
       </div>
-      <Row gutter={16} className="">
+      <Row >
         {featuredNew.map((item) => {
-          if ("children" in item) {
-            return (
-              <Col key={item.id} className=" overflow-hidden ">
-                <div className="flex flex-col h-full gap-4">
-                  {item.children.map((child) => (
-                    <div key={child.id} className="relative flex-1">
-                      <div className="absolute bottom-0 text-white p-4">
-                        <div className="tracking-[3px] uppercase">
-                          {child.title}
-                        </div>
-                        <div className="text-lg font-bold">{child.content}</div>
-                      </div>
-                      <Image
-                        src={child.img}
-                        alt="feature new"
-                        width={300}
-                        height={300}
-                        className="h-full object-cover"
-                      />
-                    </div>
-                  ))}
-                </div>
-              </Col>
-            );
-          } else {
-            return (
-              <CardImgText
-                key={item.id}
-                title={item.title}
-                content={item.content}
-                img={item.img}
-                span={item.span}
-              />
-            );
-          }
+          return (
+            <CardImgText
+              key={item.id}
+              title={item.title}
+              content={item.content}
+              img={item.img}
+              span={item.span}
+              gather={item.gather}
+            />
+          );
         })}
-      </Row>
-      <Row gutter={16} className="">
-        {featuredNew.map(
-          (item) =>
-            "gather" in item &&
-            item.gather.map((gatherItem) => (
-              <Col
-                key={gatherItem.id}
-                span={6}
-                className="pt-4 !flex !items-center"
-              >
-                <Image
-                  src={gatherItem.avatar}
-                  alt="feature new"
-                  width={80}
-                  height={80}
-                  className="rounded-full"
-                />
-                <div className="px-5 text-black">
-                  <div className="tracking-[3px] uppercase">
-                    {gatherItem.title}
-                  </div>
-                  <div className="text-base font-bold">
-                    {gatherItem.content}
-                  </div>
-                </div>
-              </Col>
-            ))
-        )}
       </Row>
     </div>
   );
