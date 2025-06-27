8409d3661c6fe7575e25b4f29a664e4c442ae2e8
Minh Vương Quang <vuongquangminh120802@gmail.com>
2025-06-27 09:34:56 +0700

    style

--- app/components/ImageOfTheDay.tsx
+++ app/components/ImageOfTheDay.tsx
@@ -38,62 +38,60 @@ export default function ImageOfTheDay({
     return () => window.removeEventListener("resize", checkWidth);
   }, []);
   return (
-    <div className="container mx-auto py-20">
-      <Row gutter={32} className="px-16 md:px-0">
-        <Col span={24} md={spanColLeft ?? 12} className="px-20">
-          <div className="md:pr-30">
-            <div className="">
-              <p className="uppercase tracking-[3px] text-sm">{badge ?? ""}</p>
-              <h2 className="text-5xl font-bold font-fantasy">{title}</h2>
-            </div>
-            <div className="my-6 md:my-24">
-              {isMobile && (
-                <div className="bg-[#f6f6f6] flex justify-center items-center">
-                  <Image
-                    src={src}
-                    alt="feature new"
-                    width={400}
-                    height={400}
-                    className="h-full object-cover"
-                  />
+    <Row gutter={32} className="px-16 md:px-0 container mx-auto py-20">
+      <Col span={24} md={spanColLeft ?? 12} className="px-20">
+        <div className="md:pr-30">
+          <div className="">
+            <p className="uppercase tracking-[3px] text-sm">{badge ?? ""}</p>
+            <h2 className="text-5xl font-bold font-fantasy">{title}</h2>
+          </div>
+          <div className="my-6 md:my-24">
+            {isMobile && (
+              <div className="bg-[#f6f6f6] flex justify-center items-center">
+                <Image
+                  src={src}
+                  alt="feature new"
+                  width={400}
+                  height={400}
+                  className="h-full object-cover"
+                />
+              </div>
+            )}
+            <h3 className="text-xl font-medium">{subTitle ?? ""}</h3>
+            <p className="text-base my-6 italic">{content}</p>
+            <a
+              href={urlButton}
+              target="_self"
+              className="flex items-center text-xl font-medium"
+            >
+              <span className="text-black">{textButton ?? ""}</span>
+              {textButton && (
+                <div className=" inline-block mx-2 bg-[#f64137] rounded-full p-1 text-base">
+                  <MoveUpRight color="white" size={14} />
                 </div>
               )}
-              <h3 className="text-xl font-medium">{subTitle ?? ""}</h3>
-              <p className="text-base my-6 italic">{content}</p>
-              <a
-                href={urlButton}
-                target="_self"
-                className="flex items-center text-xl font-medium"
-              >
-                <span className="text-black">{textButton ?? ""}</span>
-                {textButton && (
-                  <div className=" inline-block mx-2 bg-[#f64137] rounded-full p-1 text-base">
-                    <MoveUpRight color="white" size={14} />
-                  </div>
-                )}
-              </a>
-            </div>
+            </a>
           </div>
+        </div>
+      </Col>
+      {!isMobile && (
+        <Col
+          span={24}
+          md={spanColRight ?? 12}
+          className={`bg-[#f6f6f6] !flex justify-center items-center ${
+            subSrc && " flex-col"
+          }`}
+        >
+          <Image
+            src={src}
+            alt="feature new"
+            width={400}
+            height={400}
+            className="max-h-[500px] w-full object-contain"
+          />
+          <p className="py-6">{subSrc ?? ""}</p>
         </Col>
-        {!isMobile && (
-          <Col
-            span={24}
-            md={spanColRight ?? 12}
-            className={`bg-[#f6f6f6] !flex justify-center items-center ${
-              subSrc && " flex-col"
-            }`}
-          >
-            <Image
-              src={src}
-              alt="feature new"
-              width={400}
-              height={400}
-              className="max-h-[500px] w-full object-contain"
-            />
-            <p className="py-6">{subSrc ?? ""}</p>
-          </Col>
-        )}
-      </Row>
-    </div>
+      )}
+    </Row>
   );
 }
